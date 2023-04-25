819ab3c53df14e7c06b1455370d34cd31b361365
lucastamura <lucas10.tamura@gmail.com>
2023-04-25 19:50:09 +0000

    up

---
+++ app/src/main/java/tarefasspring/controllers/IndexController.java
@@ -0,0 +1,18 @@
+package tarefasspring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+;
+
+@Controller
+public class IndexController {
+    @RequestMapping("/")
+    public String home(@RequestParam("nome") String nome, Model model){
+        model.addAttribute("mensagem", "Olá " + nome);
+        return "WEB-INF/home.jsp";
+
+    }
+}
